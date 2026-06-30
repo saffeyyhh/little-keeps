e19b292a0b6e8e0721bbc55bb02c8af5605e5256
saffeyyhh <knhvts7yk8@privaterelay.appleid.com>
2026-06-30 21:36:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,79 @@
+const baseColours = [
+  { name: "Pink", value: "#ff8fab" },
+  { name: "Blue", value: "#8ecae6" },
+  { name: "Purple", value: "#cdb4db" }
+];
+
+const tileColours = [
+  { name: "Pink", value: "#ff8fab" },
+  { name: "Blue", value: "#8ecae6" },
+  { name: "White", value: "#ffffff" },
+  { name: "Black", value: "#222222" }
+];
+
+const letterColours = [
+  { name: "White", value: "#ffffff" },
+  { name: "Black", value: "#222222" }
+];
+
+let selectedBase = baseColours[0].value;
+let selectedTile = tileColours[1].value;
+let selectedLetter = letterColours[0].value;
+
+function createColourButtons(list, containerId, type) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = "";
+
+  list.forEach(colour => {
+    const button = document.createElement("button");
+    button.className = "colour-btn";
+    button.onclick = () => selectColour(type, colour.value);
+
+    const swatch = document.createElement("span");
+    swatch.className = "swatch";
+    swatch.style.background = colour.value;
+
+    const label = document.createElement("span");
+    label.innerText = colour.name;
+
+    button.appendChild(swatch);
+    button.appendChild(label);
+    container.appendChild(button);
+  });
+}
+
+function selectColour(type, value) {
+  if (type === "base") selectedBase = value;
+  if (type === "tile") selectedTile = value;
+  if (type === "letter") selectedLetter = value;
+
+  document.documentElement.style.setProperty("--base-colour", selectedBase);
+  document.documentElement.style.setProperty("--tile-colour", selectedTile);
+  document.documentElement.style.setProperty("--letter-colour", selectedLetter);
+
+  updateSelectedButtons();
+}
+
+function updateSelectedButtons() {
+  document.querySelectorAll(".colour-btn").forEach(button => {
+    button.classList.remove("selected");
+  });
+}
+
 function updatePreview() {
-  let name = document.getElementById("nameInput").value || "KAYLEE";
-  let preview = document.getElementById("previewName");
+  const name = document.getElementById("nameInput").value || "KAYLEE";
+  const preview = document.getElementById("previewName");
 
   preview.innerHTML = "";
 
   name.toUpperCase().split("").forEach(letter => {
-    let tile = document.createElement("div");
+    const tile = document.createElement("div");
     tile.className = "letter-tile";
 
-    let inner = document.createElement("div");
+    const inner = document.createElement("div");
     inner.className = "inner-tile";
 
-    let text = document.createElement("span");
+    const text = document.createElement("span");
     text.className = "letter";
     text.innerText = letter;
 
@@ -21,16 +83,10 @@ function updatePreview() {
   });
 }
 
-function chooseBase(colour) {
-  document.documentElement.style.setProperty("--base-colour", colour);
-}
+document.getElementById("nameInput").addEventListener("input", updatePreview);
 
-function chooseLetter(colour) {
-  document.documentElement.style.setProperty("--letter-colour", colour);
-}
-
-function chooseTile(colour) {
-  document.documentElement.style.setProperty("--tile-colour", colour);
-}
+createColourButtons(baseColours, "baseColours", "base");
+createColourButtons(tileColours, "tileColours", "tile");
+createColourButtons(letterColours, "letterColours", "letter");
 
 updatePreview();
